d019b5db64ceaa1508b67aaeb4ec5b3c92ec06cd
mizurex <viswasdhal@gmail.com>
2025-04-10 18:45:39 +0530

    change

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"ecom/config"
 	"ecom/handler"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db := config.ConnectDB()
-
+	fmt.Println("starting")
 	handler.SetDB(db)
 	router := gin.Default()
 	router.POST("/user", handler.AddUserHandler)
